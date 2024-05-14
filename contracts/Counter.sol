d8ddaa0a9eb25d0ec93afec2d8f5b6f8742a8be8
Enigmatic Aura <60955413+adicahyadir@users.noreply.github.com>
2024-05-14 13:22:03 +0700

    Add files via upload

---
+++ contracts/Counter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IGmpReceiver} from "contracts/IGmpReceiver.sol";
+
+contract Counter is IGmpReceiver {
+    address private immutable _gateway;
+    uint256 public number;
+
+    constructor(address gateway) {
+        _gateway = gateway;
+    }
+
+    function onGmpReceived(bytes32, uint128, bytes32, bytes calldata) external payable returns (bytes32) {
+        require(msg.sender == _gateway, "unauthorized");
+        number++;
+        return bytes32(number);
+    }
+}
